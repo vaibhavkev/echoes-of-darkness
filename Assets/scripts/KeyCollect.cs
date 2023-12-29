5fbaab9e8fc5468216f2d06d8d34404006851240
vaibhavkev <vaibhavkev@gmail.com>
2023-12-29 16:14:11 +0530

    keys

---
+++ Assets/scripts/KeyCollect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyCollect : MonoBehaviour
+{
+    private int keyno;
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyno = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = keyno.ToString() + "/5";
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            Destroy(other.gameObject);
+            keyno++;
+        }   
+    }
+}
